2f123ef7c24c419ed886d4b3640b700249c7d816
meixiaoke <m13276126840@163.com>
2024-04-18 22:33:59 +0800

    做题页面

--- src/views/system/question/doQuestion.vue
+++ src/views/system/question/doQuestion.vue
@@ -1,9 +1,29 @@
 <template>
-<h1>doQuestion</h1>
+  <h1>{{question}}</h1>
 </template>
 
 <script>
+import { listQuestion, getQuestion, delQuestion, addQuestion, updateQuestion } from '@/api/system/question'
 
+export default {
+  name: 'DoQuestion',
+  data() {
+    return {
+      question: {}
+
+    }
+  },
+  created() {
+    this.getQuestionById()
+  },
+  methods: {
+    getQuestionById() {
+      getQuestion(this.$route.params.questionId).then(response => {
+        this.question = response.data
+      })
+    }
+  }
+}
 </script>
 
 
